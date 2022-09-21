97aee36b99e69a5913db0873f5c339438f8f318a
Cristhian Aguilar <cristhian_david.aguilar@outlook.es>
2022-09-21 14:26:29 -0500

    first

---
+++ src/main/java/com/example/stockquotespring/StockQuoteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockquotespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockQuoteSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockQuoteSpringApplication.class, args);
+    }
+
+}
