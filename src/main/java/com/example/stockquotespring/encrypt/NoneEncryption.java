0f6becf5dacc628f414348bf3556067cff3c2862
Cristhian Aguilar <cristhian_david.aguilar@outlook.es>
2022-09-29 17:12:06 -0500

    adding log in none encryption

--- src/main/java/com/example/stockquotespring/encrypt/NoneEncryption.java
+++ src/main/java/com/example/stockquotespring/encrypt/NoneEncryption.java
@@ -1,6 +1,13 @@
 package com.example.stockquotespring.encrypt;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class NoneEncryption implements Encryptor {
+
+    public NoneEncryption() {
+        log.info("Not using encryption");
+    }
 
     @Override
     public String encrypt(String input) {
