74c0a5a18173c747a2f55d9a00dc1ca167f57dd7
Cristhian Aguilar <cristhian_david.aguilar@outlook.es>
2022-10-03 14:31:34 -0500

    #1 adding iv bytes in aesgsm encryptor

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/stockquote/encrypt/AESGSMEncryption.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/stockquote/encrypt/AESGSMEncryption.java
@@ -15,11 +15,12 @@ package com.ibm.hybrid.cloud.sample.stocktrader.stockquote.encrypt;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.security.crypto.util.EncodingUtils;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
@@ -31,10 +32,11 @@ import java.util.Base64;
 @Slf4j
 public class AESGSMEncryption implements Encryptor {
 
+    private static final int IV_BYTES_LENGTH = 96;
+    private static final int SALT_BYTES_LENGTH = 8;
     private static AESGSMEncryption encryption;
     private static final String GSM_ALGORITHM = "AES/GCM/NoPadding";
     private final Key key;
-    private final IvParameterSpec initialVector;
 
     public static AESGSMEncryption getInstance(Environment env) throws AESException {
         if (encryption == null)
@@ -46,16 +48,23 @@ public class AESGSMEncryption implements Encryptor {
         log.info("Using AESGSMEncryption");
         String password = env.getProperty("app.encryption.password");
         assert password != null;
-        initialVector = generateInitialVector(96);
-        key = getKeyFromPassword(password, generateSecureRandomBytes(8));
+        if (env.getProperty("app.encryption.saltBytes") != null) {
+            log.info("using with salt bytes from env");
+            key = getKeyFromPassword(password, Hex.decode(env.getProperty("app.encryption.saltBytes")));
+        } else {
+            log.info("using default constructor");
+            key = getKeyFromPassword(password, generateSecureRandomBytes(SALT_BYTES_LENGTH));
+        }
     }
 
     public String encrypt(String input) throws AESException {
         try {
+            var ivBytes = generateSecureRandomBytes(IV_BYTES_LENGTH);
             Cipher cipher = Cipher.getInstance(GSM_ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, key, getGcmParamSpecification());
+            cipher.init(Cipher.ENCRYPT_MODE, key, getGcmParamSpecification(ivBytes));
             byte[] cipherText = cipher.doFinal(input.getBytes());
-            var encodedIn64 = Base64.getEncoder().encodeToString(cipherText);
+            var cipherTextWithIvBytes = EncodingUtils.concatenate(ivBytes, cipherText);
+            var encodedIn64 = Base64.getEncoder().encodeToString(cipherTextWithIvBytes);
             log.info("encrypting in base 64 - {}", encodedIn64);
             return encodedIn64;
         } catch (Exception e) {
@@ -66,27 +75,33 @@ public class AESGSMEncryption implements Encryptor {
     public String decrypt(String cipherTextInBase64) throws AESException {
         log.info("decrypting base 64 - {}", cipherTextInBase64);
         try {
+            var cipherBytesWithIv = Base64.getDecoder().decode(cipherTextInBase64.getBytes());
+            var ivBytes = getIvBytes(cipherBytesWithIv);
             Cipher cipher = Cipher.getInstance(GSM_ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key, getGcmParamSpecification());
-            byte[] plainText = cipher.doFinal(Base64.getDecoder().decode(cipherTextInBase64.getBytes()));
+            cipher.init(Cipher.DECRYPT_MODE, key, getGcmParamSpecification(ivBytes));
+            byte[] plainText = cipher.doFinal(getCipherText(cipherBytesWithIv));
             return new String(plainText);
         } catch (Exception e) {
             throw new AESException(e.getMessage());
         }
     }
 
-    private AlgorithmParameterSpec getGcmParamSpecification() {
-        return new GCMParameterSpec(128, initialVector.getIV());
+    private byte[] getCipherText(byte[] cipherTextWithIvBytes) {
+        return EncodingUtils.subArray(cipherTextWithIvBytes, IV_BYTES_LENGTH, cipherTextWithIvBytes.length);
     }
 
-    private IvParameterSpec generateInitialVector(int byteNum) {
-        return new IvParameterSpec(generateSecureRandomBytes(byteNum));
+    private byte[] getIvBytes(byte[] cipherTextWithIvBytes) {
+        return EncodingUtils.subArray(cipherTextWithIvBytes, 0, IV_BYTES_LENGTH);
+    }
+
+    private AlgorithmParameterSpec getGcmParamSpecification(byte[] ivBytes) {
+        return new GCMParameterSpec(128, ivBytes);
     }
 
     private Key getKeyFromPassword(String password, byte[] salt) throws AESException {
         try {
             SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 256);
+            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 1024, 256);
             return new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
         } catch (Exception e) {
             throw new AESException(e.getMessage());
